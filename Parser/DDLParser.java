c5e2caf3b16b7d9137ef22c7dd9d6a8c3e10b9d2
Ejeffers1239 <eej4105@g.rit.edu>
2024-02-01 09:13:39 -0500

    Update DDLParser.java
    Tokenizer made

--- Parser/DDLParser.java
+++ Parser/DDLParser.java
@@ -1,11 +1,12 @@
 package Parser;
+import java.util.Scanner;
 
 
 public class DDLParser {
 
 
     public static void parseCreateTable(String ddlStatement) {
-
+		
     }
 
     public static void parseDropTable(String ddlStatement) {
@@ -15,4 +16,41 @@ public class DDLParser {
     public static void parseAlterTable(String ddlStatement) {
 
     }
+	
+	private Arraylist<String> Tokenize(String ddlStatement){
+		Scanner scanner = new scanner(ddlStatement);
+		Arraylist<String> tokens;
+		String currentToken = ""
+		char nextByte (Char)scanner.nextByte();
+		bool sentinal = false
+		While(scanner.hasNext){
+			if(nextByte = '(' && sentinal){
+				tokens.add("(")
+				nextByte = scanner.nextByte();
+			}
+			else if(nextByte = ')' && sentinal){
+				tokens.add(")")
+				nextByte = scanner.nextByte();
+			}
+			else if(nextByte = ';' && sentinal){
+				tokens.add(";")
+				nextByte = scanner.nextByte();
+			}
+			else if(nextByte = ',' && sentinal){
+				tokens.add(",")
+				nextByte = scanner.nextByte();
+			}
+			else if(Character.isLetterOrDigit(nextByte)){
+				CurrentToken += nextByte;
+				sentinal = true;
+			} else{
+				tokens.add(currentToken);
+				currentToken = "";
+				sentinal = false;
+				nextByte = scanner.nextByte();
+			}
+		}
+			
+		
+	}
 }
