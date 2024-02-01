c8e3f892ea3bd7a2799a5b6e9168cfd7044bd6e2
Chen Lin <cl5613@rit.edu>
2024-02-01 08:58:18 -0500

    Updated DMLParser

--- Parser/DMLParser.java
+++ Parser/DMLParser.java
@@ -1,6 +1,19 @@
 package Parser;
 
+import StorageManager.Objects.Catalog;
+import StorageManager.Objects.Page;
+import StorageManager.Objects.Table;
+import StorageManager.StorageManager;
+import StorageManager.TableSchema;
+
 public class DMLParser {
+
+    private static Catalog catalog;
+    private static StorageManager storageManager;
+    private static Table table;
+    private static Page page;
+    private static TableSchema tableSchema;
+
 
     public static void parseInsert(String dmlStatement) {
         // Format: insert into <name> values <tuples>;
@@ -9,14 +22,11 @@ public class DMLParser {
         String[] parts = dmlStatement.split(" ");
         if (parts.length < 4 || !parts[0].equalsIgnoreCase("insert")
                 || !parts[1].equalsIgnoreCase("into")
-                || !parts[3].equalsIgnoreCase("values")) {
+                || !parts[3].equalsIgnoreCase("values"))
+        {
             throw new IllegalArgumentException("Incorrect insert command");
-
         }
-
         String tableName = parts[2];
-
-
     }
 
     public static void parseSelect(String dmlStatement) {
@@ -25,7 +35,8 @@ public class DMLParser {
 
         String[] parts = dmlStatement.split(" ");
         if (parts.length < 4 || !parts[0].equalsIgnoreCase("select")
-                             || !parts[2].equalsIgnoreCase("from")) {
+                             || !parts[2].equalsIgnoreCase("from"))
+        {
             throw new IllegalArgumentException("Incorrect select command");
         }
 
@@ -35,32 +46,33 @@ public class DMLParser {
         // Command: display schema
         // It will show: database location, page size, buffer, size, and table schema
         // Command: display info <name>
-        // It will show: table name, table schema, number of pages, number of records
+        // It will show: table name, table schema, # of pages, # of records
 
         String[] parts = dmlStatement.split(" ");
 
-        if (parts.length < 3 || !parts[0].equalsIgnoreCase("display")) {
+        if (parts.length < 2 || parts.length > 3 || !parts[0].equalsIgnoreCase("display"))
+        {
             throw new IllegalArgumentException("Incorrect display command");
         }
 
-        if (parts[1].equalsIgnoreCase("schema")) {
-            System.out.println("Database Location: " + " ");
-            System.out.println("Page Size: " + " ");
-            System.out.println("Buffer Size: " + " ");
-            System.out.println("Table Schema: " + " ");
+        if (parts[1].equalsIgnoreCase("schema"))
+        {
+            System.out.println("Database Location: " + catalog.getDbLocation());
+            System.out.println("Page Size: " + catalog.getPageSize());
+            System.out.println("Buffer Size: " + catalog.getBufferSize());
+            System.out.println("Table Schema: " + catalog.getSchemas());
         }
-        else if (parts[1].equalsIgnoreCase("info") && parts.length == 3) {
-            String tableName = parts[2];
-
-            System.out.println("Table Name: " + tableName);
-            System.out.println("Table Schema: " + " ");
-            System.out.println("Number of Pages: " + " ");
-            System.out.println("Number of Records: " + " ");
+        else if (parts[1].equalsIgnoreCase("info") && parts.length == 3)
+        {
+            System.out.println("Table Name: " + tableSchema.getTableName());
+            System.out.println("Table Schema: " + catalog.getSchemas());
+            System.out.println("Number of Pages: " + table.getNumPages());
+            System.out.println("Number of Records: " + page.getNumRecords());
         }
-        else {
+        else
+        {
             System.out.println("Invalid Display Command");
         }
-
     }
 
     public static void parseDelete(String dmlStatement) {
