c58b999a4d2855fe4a539d31d2480bb30492fbd3
Ejeffers1239 <eej4105@g.rit.edu>
2024-02-04 13:00:24 -0500

    Update DDLParser.java
    missed some semicolons

--- Parser/DDLParser.java
+++ Parser/DDLParser.java
@@ -2,7 +2,7 @@ package Parser;
 import java.util.Scanner;
 
 
-public class DDLParser extends Parser{
+public static class DDLParser extends Parser{
 
     public static void parseCreateTable(String ddlStatement) 
 	{	
@@ -86,89 +86,4 @@ public class DDLParser extends Parser{
 
     }
 	
-	private boolean keywordCheck(String label)
-	{
-		label = label.toLower()
-		return (label.equals("integer") ||
-				label.equals("double") ||
-				label.equals("boolean") ||
-				label.equals("char") ||
-				label.equals("varchar") ||
-				label.equals("notnull") ||
-				label.equals("primarykey") ||
-				label.equals("unique") ||
-				label.equals("distinct") ||
-				label.equals("table") ||
-				label.equals("create") ||
-				label.equals("drop") ||
-				label.equals("alter") ||
-				label.equals("select")
-				label.equals("into") ||
-				label.equals("values") ||
-				label.equals("add") ||
-				label.equals("default"));
-	}
-	
-	private Arraylist<String> Tokenize(String ddlStatement)
-	{
-        Scanner scanner = new scanner(ddlStatement);
-        Arraylist<String> tokens;
-        String currentToken = "";
-        char nextByte (Char)scanner.nextByte();
-        bool label = false;
-		bool number = false;
-		bool sentinal = (!label && ! number);
-		bool hasdecimal = false;
-        While(scanner.hasNext){
-            if(nextByte == '(' && !sentinal)
-			{
-                tokens.add("(")
-                nextByte = scanner.nextByte();
-            }
-            else if(nextByte == ')' && !sentinal)
-			{
-                tokens.add(")")
-                nextByte = scanner.nextByte();
-            }
-            else if(nextByte == ';' && !sentinal)
-			{
-                tokens.add(";")
-                nextByte = scanner.nextByte();
-            }
-            else if(nextByte == ',' && !sentinal)
-			{
-                tokens.add(",")
-                nextByte = scanner.nextByte();
-            }
-			else if((Character.isDigit(nextByte) || (nextByte == "." && !hasdecimal)) && !label) //only ONE decimal point per double!
-			{
-				if(nextByte == ".")
-				{
-					hasdecimal = true;
-				}
-				currentToken += nextByte;
-				number = true;
-				nextByte = scanner.nextByte();
-			}
-            else if(Character.isLetterOrDigit(nextByte)) //covering both labels and values in the same block since values only come after "default" anyway
-			{
-				if(!label && !Character.isLetter(nextByte){
-					throw new Exception("Labels must start with a letter!") //realistically the digit branch should stop this from ever being accessible
-				}
-                currentToken += nextByte;
-                label = true; //we need to block the other paths to know when to flush.
-                nextByte = scanner.nextByte();
-            } else
-			{ //flush the label or number token string
-                tokens.add(currentToken);
-                currentToken = "";
-                number = false;
-				label = false;
-				hasdecimal = false;
-            } else 
-			{
-				throw new Exception("invalid token, please check your command")
-			}
-        }            
-    }
 }
