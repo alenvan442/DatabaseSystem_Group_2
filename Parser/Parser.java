cdd93c358cea0dcc243e4977f82cd8cf427044df
Ejeffers1239 <eej4105@g.rit.edu>
2024-02-06 09:07:35 -0500

    Even more bugfixes

--- Parser/Parser.java
+++ Parser/Parser.java
@@ -1,12 +1,12 @@
 //contains methods common to DDLParser and DMLParser
 package Parser;
 
-public static class Parser{ //extend me!
-	
-	//keywordCheck returns false if the passed label is a protected keyword, true otherwise
-	private boolean keywordCheck(String label)
-	{
-		label = label.toLower()
+public static class Parser { // extend me!
+
+	// keywordCheck returns false if the passed label is a protected keyword, true
+	// otherwise
+	private boolean keywordCheck(String label) {
+		label = label.toLower();
 		return !(label.equals("integer") ||
 				label.equals("double") ||
 				label.equals("boolean") ||
@@ -20,81 +20,75 @@ public static class Parser{ //extend me!
 				label.equals("create") ||
 				label.equals("drop") ||
 				label.equals("alter") ||
-				label.equals("select")
+				label.equals("select") ||
 				label.equals("into") ||
 				label.equals("values") ||
 				label.equals("add") ||
 				label.equals("default"));
 	}
-	
-	//Tokenize Splits the ddlStatement into a list of strings, while checking some light semantics. There are a few types
-	//"("
-	//")"
-	//","
-	//";"
-	//Labels, which are alphanumeric and start with a letter, ths includes keywords such as "Create"
-	//Numbers, which can be integer or double with exactly 1 "." in any place, the char/varchar case must check for double seperately.
-	//any tokens outside these options will throw an error.
-	private Arraylist<String> Tokenize(String ddlStatement)
-	{
-        Scanner scanner = new scanner(ddlStatement);
-        Arraylist<String> tokens;
-        String currentToken = "";
-        char nextByte (Char)scanner.nextByte();
-        bool label = false;
+
+	// Tokenize Splits the ddlStatement into a list of strings, while checking some
+	// light semantics. There are a few types
+	// "("
+	// ")"
+	// ","
+	// ";"
+	// Labels, which are alphanumeric and start with a letter, ths includes keywords
+	// such as "Create"
+	// Numbers, which can be integer or double with exactly 1 "." in any place, the
+	// char/varchar case must check for double seperately.
+	// any tokens outside these options will throw an error.
+	private Arraylist<String> Tokenize(String ddlStatement) {
+		Scanner scanner = new scanner(ddlStatement);
+		Arraylist<String> tokens;
+		String currentToken = "";
+		char nextByte = (Char) scanner.nextByte();
+		bool label = false;
 		bool number = false;
-		bool sentinal = (!label && ! number);
+		bool sentinal = (!label && !number);
 		bool hasdecimal = false;
-        While(scanner.hasNext){
-            if(nextByte == '(' && !sentinal)
+		while (scanner.hasNext()) {
+			if (nextByte == '(' && !sentinal) {
+				tokens.add("(");
+				nextByte = scanner.nextByte();
+			} else if (nextByte == ')' && !sentinal) {
+				tokens.add(")");
+				nextByte = scanner.nextByte();
+			} else if (nextByte == ';' && !sentinal) {
+				tokens.add(";");
+				nextByte = scanner.nextByte();
+			} else if (nextByte == ',' && !sentinal) {
+				tokens.add(",");
+				nextByte = scanner.nextByte();
+			} else if ((Character.isDigit(nextByte) || (nextByte == "." && !hasdecimal)) && !label) // only ONE decimal
+																									// point per double!
 			{
-                tokens.add("(")
-                nextByte = scanner.nextByte();
-            }
-            else if(nextByte == ')' && !sentinal)
-			{
-                tokens.add(")")
-                nextByte = scanner.nextByte();
-            }
-            else if(nextByte == ';' && !sentinal)
-			{
-                tokens.add(";")
-                nextByte = scanner.nextByte();
-            }
-            else if(nextByte == ',' && !sentinal)
-			{
-                tokens.add(",")
-                nextByte = scanner.nextByte();
-            }
-			else if((Character.isDigit(nextByte) || (nextByte == "." && !hasdecimal)) && !label) //only ONE decimal point per double!
-			{
-				if(nextByte == ".")
-				{
+				if (nextByte == ".") {
 					hasdecimal = true;
 				}
 				currentToken += nextByte;
 				number = true;
 				nextByte = scanner.nextByte();
-			}
-            else if(Character.isLetterOrDigit(nextByte)) //covering both labels and values in the same block since values only come after "default" anyway
+			} else if (Character.isLetterOrDigit(nextByte)) // covering both labels and values in the same block since
+															// values only come after "default" anyway
 			{
-				if(!label && !Character.isLetter(nextByte)){
-					throw new Exception("Labels must start with a letter!"); //realistically the digit branch should stop this from ever being accessible
+				if (!label && !Character.isLetter(nextByte)) {
+					throw new Exception("Labels must start with a letter!"); // realistically the digit branch should
+																				// stop this from ever being accessible
 				}
-                currentToken += nextByte;
-                label = true; //we need to block the other paths to know when to flush.
-                nextByte = scanner.nextByte();
-            } else
-			{ //flush the label or number token string
-                tokens.add(currentToken);
-                currentToken = "";
-                number = false;
+				currentToken += nextByte;
+				label = true; // we need to block the other paths to know when to flush.
+				nextByte = scanner.nextByte();
+			} else { // flush the label or number token string
+				if (currentToken.equals("")) {
+					throw new Exception("Invalid Token!");
+				}
+				tokens.add(currentToken);
+				currentToken = "";
+				number = false;
 				label = false;
 				hasdecimal = false;
-            } else 
-			{
-				throw new Exception("invalid token, please check your command");
 			}
-        }            
-    }
+		}
+	}
 }
