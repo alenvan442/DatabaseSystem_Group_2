4575aaa531171de047e487b12eed356a276a0653
Chen Lin <cl5613@rit.edu>
2024-02-15 09:40:32 -0500

    Fixed some errors in DMLParser

--- Parser/DMLParser.java
+++ Parser/DMLParser.java
@@ -1,12 +1,15 @@
 package Parser;
 
+import StorageManager.Objects.AttributeSchema;
 import StorageManager.Objects.Catalog;
 import StorageManager.Objects.Page;
 import StorageManager.Objects.Table;
 import StorageManager.StorageManager;
 import StorageManager.TableSchema;
 
-public class DMLParser {
+import java.util.ArrayList;
+
+public class DMLParser extends ParserCommon{
 
     private static Catalog catalog;
     private static StorageManager storageManager;
@@ -14,22 +17,38 @@ public class DMLParser {
     private static Page page;
     private static TableSchema tableSchema;
 
-
-    public static void parseInsert(String dmlStatement) {
+    public static void parseInsert(String dmlStatement) throws Exception {
         // Format: insert into <name> values <tuples>;
         // Example: insert into foo values (1 "foo" true 2.1);
+        ArrayList<AttributeSchema> attributes = new ArrayList<>();
 
-        String[] parts = dmlStatement.split(" ");
-        if (parts.length < 4 || !parts[0].equalsIgnoreCase("insert")
-                || !parts[1].equalsIgnoreCase("into")
-                || !parts[3].equalsIgnoreCase("values"))
-        {
-            throw new IllegalArgumentException("Incorrect insert command");
+        ArrayList<String> tokens = Tokenize(dmlStatement);
+        if (tokens.size() != 5) {
+            throw new IllegalArgumentException("Insert command should be five arguments in total");
         }
-        String tableName = parts[2];
+        if (!tokens.getFirst().equalsIgnoreCase("insert")
+                && !tokens.get(1).equalsIgnoreCase("into")
+                && !tokens.get(2).equals("values")) {
+            throw new IllegalArgumentException("Incorrect insert command. Check it. ");
+        }
+
+        String tableName;
+
+        //if (tableName.charAt(0) == "A") {
+
+        //}
+
+
+        // get the Catalog, access the map include the schema,
+            // attributes have the match the one in table, else throw error
+        // access tableName to get the attribute
+
+        // insert, update, check if values are passed in
+
+
     }
 
-    public static void parseSelect(String dmlStatement) {
+    public static void parseSelect(String dmlStatement) throws Exception{
         // Command: select * from <name>;
         // Example: select * from foo;
 
@@ -39,30 +58,29 @@ public class DMLParser {
         {
             throw new IllegalArgumentException("Incorrect select command");
         }
+        //TODO
 
     }
 
-    public static void parseDisplay(String dmlStatement) {
+    public static void parseDisplay(String dmlStatement) throws Exception {
         // Command: display schema
         // It will show: database location, page size, buffer, size, and table schema
         // Command: display info <name>
         // It will show: table name, table schema, # of pages, # of records
 
-        String[] parts = dmlStatement.split(" ");
+        ArrayList<String> tokens = Tokenize(dmlStatement);
 
-        if (parts.length < 2 || parts.length > 3 || !parts[0].equalsIgnoreCase("display"))
-        {
-            throw new IllegalArgumentException("Incorrect display command");
-        }
-
-        if (parts[1].equalsIgnoreCase("schema"))
+        if (tokens.getFirst().equalsIgnoreCase("display")
+                && tokens.get(1).equalsIgnoreCase("schema"))
         {
             System.out.println("Database Location: " + catalog.getDbLocation());
             System.out.println("Page Size: " + catalog.getPageSize());
             System.out.println("Buffer Size: " + catalog.getBufferSize());
             System.out.println("Table Schema: " + catalog.getSchemas());
         }
-        else if (parts[1].equalsIgnoreCase("info") && parts.length == 3)
+        else if (tokens.getFirst().equalsIgnoreCase("display")
+                && tokens.get(1).equalsIgnoreCase("info")
+                && tokens.size() == 3)
         {
             System.out.println("Table Name: " + tableSchema.getTableName());
             System.out.println("Table Schema: " + catalog.getSchemas());
@@ -71,7 +89,7 @@ public class DMLParser {
         }
         else
         {
-            System.out.println("Invalid Display Command");
+            throw new Exception("Invalid Display Command or Incorrect table name");
         }
     }
 
@@ -80,6 +98,6 @@ public class DMLParser {
     }
 
     public static void parseUpdate(String dmlStatement) {
-
+        // delete / insert record
     }
 }
