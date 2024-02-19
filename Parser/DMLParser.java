80c35bdc41ce546ccd173e2806868775d2e6af3f
Chen Lin <cl5613@rit.edu>
2024-02-19 11:09:53 -0500

    changes to parseSelect

--- Parser/DMLParser.java
+++ Parser/DMLParser.java
@@ -9,7 +9,7 @@ import StorageManager.Objects.MessagePrinter.MessageType;
 import StorageManager.StorageManager;
 import StorageManager.TableSchema;
 
-import java.awt.*;
+import javax.management.Attribute;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.List;
@@ -18,75 +18,73 @@ public class DMLParser extends ParserCommon{
 
     public static void parseInsert(String dmlStatement) throws Exception {
         // Format: insert into <name> values <tuples>;
-
         ArrayList<AttributeSchema> attributes = new ArrayList<>();
         Catalog catalog = Catalog.getCatalog();
+        String tableName;
 
         ArrayList<String> tokens = Tokenize(dmlStatement);
-        if (tokens.size() != 5) {
-            throw new IllegalArgumentException("Insert command should be five arguments in total");
-        }
+
         if (!tokens.getFirst().equalsIgnoreCase("insert")
                 && !tokens.get(1).equalsIgnoreCase("into")
                 && !tokens.get(3).equalsIgnoreCase("values"))
         {
             throw new IllegalArgumentException("Incorrect insert command. Check it. ");
         }
-
-        String tableName = tokens.get(2).toLowerCase();
-
-        // create list of tuples.. ((1, "  ", 2, 3), (2, " ", 3, 4)...)
+        if (Character.isLetter(tokens.get(2).charAt(0)) && keywordCheck(tokens.get(2))) {
+            tableName = tokens.get(2).toLowerCase();
+        }
+        else {
+            throw new Exception("table name must all be alphabets");
+        }
+        /**
         List<List<String>> tuples = new ArrayList<>();
         for (List<String> tuple: tuples) {
             if (!tuple.getFirst().equals("(")|| !tuple.contains(",") || !tuple.get(2).startsWith("\""))
             {
-                throw new IllegalArgumentException("Incorrect tuple format");
             }
+        }**/
+        int i = 4;
+        while (!tokens.get(i).equals(")") && i < 99999)
+        {
+
 
         }
-        /**
-        for (TableSchema schema: catalog.getSchemas().values()) {
-            if (schema.getTableName().contains(tableName)) {
-
-            }
-        }
-         **/
         // TODO
-
         // get the Catalog, access the map include the schema,
         // attributes have the match the one in table, else throw error
         // access tableName to get the attribute
-
         // insert, update, check if values are passed in
 
     }
 
-    public static void parseSelect(String dmlStatement) throws Exception{
+    public static ArrayList<String> parseSelect(String dmlStatement) throws Exception{
         // Format: select * from <name>;
-
         ArrayList<String> tokens = Tokenize(dmlStatement);
         Catalog catalog = Catalog.getCatalog();
+        ArrayList<String> attributes = new ArrayList<>();
+        String tableName;
 
         if (tokens.size() != 4 || !tokens.getFirst().equalsIgnoreCase("select")
                              || !tokens.get(1).equalsIgnoreCase("*")
                             || !tokens.get(2).equals("from")
                             || tokens.get(3).isEmpty())
         {
-            throw new IllegalArgumentException("Incorrect select command");
+            MessagePrinter.printMessage(MessageType.ERROR, "incorrect format");
         }
         else
-        {
-            String tableName = tokens.get(3).toLowerCase();
-            ArrayList<AttributeSchema> attributes = new ArrayList<>();
+        {   // if the command is correct, check if table name exists
+            tableName = tokens.get(3).toLowerCase();
             for (TableSchema schema: catalog.getSchemas().values()) {
-                if (schema.getTableName().contains(tableName)){
-
-                    //TODO
+                if (!schema.getTableName().contains(tableName)){
+                    MessagePrinter.printMessage(MessageType.ERROR, "table not found");
+                }
+                else
+                {
+                    attributes.add(String.valueOf(schema.getAttributes()));
                 }
             }
-
         }
-
+        return attributes;
     }
 
     public static void parseDisplaySchema(ArrayList<String> tokens) throws Exception {
@@ -101,15 +99,17 @@ public class DMLParser extends ParserCommon{
         // It will show: database location, page size, buffer, size, and table schema
         // Command: display info <name>
         // It will show: table name, table schema, # of pages, # of records
+        String tableName;
 
         if (tokens.get(0).equalsIgnoreCase("display")
         && tokens.get(1).equalsIgnoreCase("info")
         && tokens.get(3).equals(";")
         && tokens.size() == 4)
         {
-            String tableName = tokens.get(2).toLowerCase();
+            tableName = tokens.get(2).toLowerCase();
             return tableName;
-        } else {
+        }
+        else {
             MessagePrinter.printMessage(MessageType.ERROR, "incorrect format");
             return null;
         }
