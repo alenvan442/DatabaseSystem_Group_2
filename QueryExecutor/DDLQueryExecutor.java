ecef82cdb18fd66c3b1fa472f4b7f85a898c5d04
sam8031 <sam8031@g.rit.edu>
2024-02-20 00:09:42 -0500

    Create DDLQueryExecutor.java

---
+++ QueryExecutor/DDLQueryExecutor.java
@@ -0,0 +1,11 @@
+package QueryExecutor;
+
+public class DDLQueryExecutor implements QueryExecutorInterface{
+
+  @Override
+  public void excuteQuery() throws Exception {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'excuteQuery'");
+  }
+
+}
