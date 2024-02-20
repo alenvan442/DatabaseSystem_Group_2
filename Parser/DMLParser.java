aaec651eb134957624c27f59b18f4dbd8e355a68
Chen Lin <cl5613@rit.edu>
2024-02-20 09:08:17 -0500

    changes to parseSelect

--- Parser/DMLParser.java
+++ Parser/DMLParser.java
@@ -1,25 +1,22 @@
 package Parser;
 
+import StorageManager.Objects.MessagePrinter;
+import StorageManager.Objects.Record;
+import StorageManager.Objects.MessagePrinter.MessageType;
 import StorageManager.Objects.AttributeSchema;
 import StorageManager.Objects.Catalog;
-import StorageManager.Objects.MessagePrinter;
-import StorageManager.Objects.Page;
-import StorageManager.Objects.Table;
-import StorageManager.Objects.MessagePrinter.MessageType;
-import StorageManager.StorageManager;
 import StorageManager.TableSchema;
-
-import javax.management.Attribute;
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.List;
+import javafx.util.Pair;
+
 
 public class DMLParser extends ParserCommon{
 
-    public static void parseInsert(String dmlStatement) throws Exception {
+    public static Pair<String, Record> parseInsert(String dmlStatement) throws Exception {
         // Format: insert into <name> values <tuples>;
-        ArrayList<AttributeSchema> attributes = new ArrayList<>();
-        Catalog catalog = Catalog.getCatalog();
-        String tableName;
+        // Catalog catalog = Catalog.getCatalog();
+        String tableName = "";
 
         ArrayList<String> tokens = Tokenize(dmlStatement);
 
@@ -27,26 +24,26 @@ public class DMLParser extends ParserCommon{
                 && !tokens.get(1).equalsIgnoreCase("into")
                 && !tokens.get(3).equalsIgnoreCase("values"))
         {
-            throw new IllegalArgumentException("Incorrect insert command. Check it. ");
+            MessagePrinter.printMessage(MessageType.ERROR, "incorrect insert format");
         }
-        if (Character.isLetter(tokens.get(2).charAt(0)) && keywordCheck(tokens.get(2))) {
+        if (Character.isLetter(tokens.get(2).charAt(0))
+                && keywordCheck(tokens.get(2)))
+        {
             tableName = tokens.get(2).toLowerCase();
         }
         else {
-            throw new Exception("table name must all be alphabets");
+            MessagePrinter.printMessage(MessageType.ERROR, "table name must be alphabets");
         }
 
-        int i = 4;
-        while (!tokens.get(i).equals(")") && i < 99999)
-        {
+        List<Object> records = new ArrayList<>();
 
-
-        }
-        // TODO
+        // might need to do the following
         // get the Catalog, access the map include the schema,
         // attributes have the match the one in table, else throw error
         // access tableName to get the attribute
         // insert, update, check if values are passed in
+
+        return new Pair<>(tableName, new Record(records));
     }
 
     public static String parseSelect(ArrayList<String> tokens) throws Exception{
@@ -59,17 +56,21 @@ public class DMLParser extends ParserCommon{
                             || tokens.get(3).isEmpty()
                             || tokens.get(4).equals(";"))
         {
-            MessagePrinter.printMessage(MessageType.ERROR, "incorrect format");
+            MessagePrinter.printMessage(MessageType.ERROR, "incorrect select format");
         }
-        else
-        {   // if the command is correct, return table name
-            tableName = tokens.get(3).toLowerCase();
+        else {
+            // if the command is correct, return table name
+            if (Character.isLetter(tokens.get(3).charAt(0))
+                    && keywordCheck(tokens.get(3))) {
+                tableName = tokens.get(3).toLowerCase();
+            }
         }
         return tableName;
     }
 
-
     public static void parseDisplaySchema(ArrayList<String> tokens) throws Exception {
+        // Command: display schema
+        // It will show: database location, page size, buffer, size, and table schema
         if (!tokens.get(0).equalsIgnoreCase("display")
                 && !tokens.get(1).equalsIgnoreCase("schema")
                 && !tokens.get(2).equals(";"))
@@ -79,8 +80,6 @@ public class DMLParser extends ParserCommon{
     }
 
     public static String parseDisplayInfo(ArrayList<String> tokens) throws Exception {
-        // Command: display schema
-        // It will show: database location, page size, buffer, size, and table schema
         // Command: display info <name>
         // It will show: table name, table schema, # of pages, # of records
         String tableName;
@@ -94,7 +93,7 @@ public class DMLParser extends ParserCommon{
             return tableName;
         }
         else {
-            MessagePrinter.printMessage(MessageType.ERROR, "incorrect format");
+            MessagePrinter.printMessage(MessageType.ERROR, "incorrect display info format");
             return null;
         }
     }
