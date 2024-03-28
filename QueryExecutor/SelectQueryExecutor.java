9ebf0bd4142a62fb3eba0bcd073992320bf8f44a
Carlos Hargrove <csh2830@rit.edu>
2024-03-28 08:59:05 -0400

    Duo-Nested Loop Error Fix in the Attr Schema

--- QueryExecutor/SelectQueryExecutor.java
+++ QueryExecutor/SelectQueryExecutor.java
@@ -175,7 +175,7 @@ public class SelectQueryExecutor implements QueryExecutorInterface {
       for(int i=0; i<cartSchemas.size(); i++){
           List<AttributeSchema> currentAttrSchema = cartSchemas.get(i).getAttributes();
           for(int j=0; j<currentAttrSchema.size(); j++){
-              String name = tableNames.get(i)+"."+currentAttrSchema.get(i).getAttributeName();
+              String name = tableNames.get(i)+"."+currentAttrSchema.get(j).getAttributeName();
               temp.addAttribute(new AttributeSchema(name, currentAttrSchema.get(i)));
 
         }
