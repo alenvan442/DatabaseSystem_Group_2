8ada218f91c4ba5b0191eb2b247b86f32ce9ceaf
Ejeffers1239 <eej4105@g.rit.edu>
2024-03-28 08:25:39 -0400

    Update Database.java
    spelling

--- Database.java
+++ Database.java
@@ -19,7 +19,7 @@ public class Database {
     }
 
     public void start() throws Exception {
-        System.out.println("Welcolm to CASE-C QL");
+        System.out.println("Welcome to CASE-C QL");
         System.out.println("Looking at " + dbLocation);
         File dbDirectory = new File(dbLocation);
         File schemaFile = new File(dbDirectory.getAbsolutePath().concat("/schema"));
