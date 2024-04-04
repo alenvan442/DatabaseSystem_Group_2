e59b8410d557f1bb782743a412ed481b0bb37ce9
Ejeffers1239 <eej4105@g.rit.edu>
2024-04-04 09:02:07 -0400

    Update InternalNode.java
    stub for internal node

--- StorageManager/BPlusTree/InternalNode.java
+++ StorageManager/BPlusTree/InternalNode.java
@@ -1,5 +1,30 @@
 package StorageManager.BPlusTree;
 
+
+import Parser.Token;
+import java.util.ArrayList;
+
 public class InternalNode implements BPlusNode {
-    
+    private ArrayList<BPlusNode> children;
+    private int n;
+
+    /**
+     * type size times pageSize = n
+     * @param PKType - The type (integer, double, char, varchar, bool?) of the primary key, implying data size
+     * @param pageSize - Size of the page
+     */
+    public InternalNode(Token PKType, int pageSize){
+
+    }
+
+    /**
+     * comparison function which searches for the value's place among its child nodes.
+     * @param value
+     * @returns the found node, for an insert or without a match, this is the node BEFORE the value's place.
+     */
+    public BPlusNode search(Token value){
+        return null;
+    }
+
 }
+
