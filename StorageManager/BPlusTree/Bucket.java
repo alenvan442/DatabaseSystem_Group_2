23e2b7c14b2ea7a2ebaaffce08159a9f7bdb686b
Carlos Hargrove <csh2830@rit.edu>
2024-04-11 09:02:39 -0400

    BucketSize calculate finished and commented. Mostly done with Bucket. Need to find out how many other features need to be implemented into it.

--- StorageManager/BPlusTree/Bucket.java
+++ StorageManager/BPlusTree/Bucket.java
@@ -1,11 +1,22 @@
 package StorageManager.BPlusTree;
 
+import StorageManager.Objects.Catalog;
+
+import static java.lang.Math.floor;
+
 public class Bucket {
 
     private int pageNumber;
 
-    private int index; //TODO: Double Check datatype.
+    private int index;
 
+    private Bucket nextBucket;
+
+    /**
+     * Soecial type of page
+     * @param pageNumber - corresponding page #
+     * @param index - index on the page for the bucket.
+     */
     public Bucket(int pageNumber, int index){
         this.pageNumber=pageNumber;
         this.index=index;
@@ -19,10 +30,24 @@ public class Bucket {
         return pageNumber;
     }
 
+    /**
+     * calculates the size of the bucket
+     * @return - size of the bucket
+     */
     public double calculateBucketSize(){
-        int floor; //TODO: double check
-        int pageSize; //TODO: Find page size
+        Catalog catalog = Catalog.getCatalog();
+        //int floor;
+        int pageSize;
+        pageSize = catalog.getPageSize();
 
     return floor (pageSize / (Integer.BYTES + Integer.BYTES) );
+    }
+
+    /**
+     * Saves the next bucket in the B+ tree to be found.
+     * @param b - the next bucket in the B+ tree.
+     */
+    public void assignNextBucket(Bucket b){
+        nextBucket = b;
     }
 }
