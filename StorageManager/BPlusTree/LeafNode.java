20ddf147db6c7afc6f83fc986abcdf4359ed2bcf
Carlos Hargrove <csh2830@rit.edu>
2024-04-12 17:01:49 -0400

    Generic Node in case we need it and added data to leaf node could probably be removed but we will see.

--- StorageManager/BPlusTree/LeafNode.java
+++ StorageManager/BPlusTree/LeafNode.java
@@ -1,5 +1,10 @@
 package StorageManager.BPlusTree;
 
-public class LeafNode {
+public class LeafNode implements Node{
+    Bucket bucket;
+
+    public LeafNode(int pageNumber, int index){
+        bucket= new Bucket(pageNumber, index);
+    }
     
 }
---
+++ StorageManager/BPlusTree/Node.java
@@ -0,0 +1,4 @@
+package StorageManager.BPlusTree;
+
+public interface Node {
+}
