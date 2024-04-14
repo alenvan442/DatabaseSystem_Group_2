76eafac3c90bf66f9234f50f6080a4dd7cb914fa
sam8031 <sam8031@g.rit.edu>
2024-04-14 12:00:07 -0400

    catalog has new indexing attribute

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
   public static void main(String[] args) {
-    if (args.length != 3) {
-      System.err.println("java Main <db_loc> <page_size> <buffer_size>");
+    if (args.length != 4) {
+      System.err.println("java Main <db_loc> <page_size> <buffer_size> <indexing>");
       System.exit(0);
     } else {
       String dbLocation = args[0];
       int pageSize =  Integer.parseInt(args[1]);
       int bufferSize = Integer.parseInt(args[2]);
+      boolean indexing = Boolean.parseBoolean(args[3]);
       Database database = new Database(dbLocation, pageSize, bufferSize);
       try {
         database.start();
